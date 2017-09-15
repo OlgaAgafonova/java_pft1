5226abffe0fca2c0f90c9f80f3c85269dfb82f67
Agafonova Olga <ol_ka.90@mail.ru>
2017-09-15 15:31:42 +0400

    добавлен класс NewUer + сделан рефакторинг

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserData.java
@@ -0,0 +1,55 @@
+package ru.stqa.pft.addressbook;
+
+public class UserData {
+    private final String firstname;
+    private final String middlename;
+    private final String lastname;
+    private final String nickname;
+    private final String company;
+    private final String address;
+    private final String home;
+    private final String mobile;
+
+    public UserData(String firstname, String middlename, String lastname, String nickname, String company, String address, String home, String mobile) {
+        this.firstname = firstname;
+        this.middlename = middlename;
+        this.lastname = lastname;
+        this.nickname = nickname;
+        this.company = company;
+        this.address = address;
+        this.home = home;
+        this.mobile = mobile;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getMiddlename() {
+        return middlename;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getHome() {
+        return home;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+}
